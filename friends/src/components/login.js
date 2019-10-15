3cc60faf6cd43217e303d8ea5c4bb12e47cd1bb1
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-15 14:43:14 +0100

    Modify <login /> component
    In this commit, I modified the <Login /> component and allow it to make an axios post request to the login endpoint of the api. The token returned is then stored in local storagefor future Auth requests

--- friends/src/components/login.js
+++ friends/src/components/login.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React,{ useRef } from 'react';
+import axios from 'axios';
 
-export default function Login(){
+export default function Login(props) {
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+
+    const submit = () => {
+        axios.post('http://localhost:5000/api/login', {
+          username: usernameRef.current.value,
+          password: passwordRef.current.value,
+        })
+          .then(res => {
+            debugger
+            localStorage.setItem('token', res.data.payload)
+            props.history.push('/friends');
+          })
+          .catch(error => {
+            alert(error.response.data.message);
+          });
+      };
 
     return(
         <div>
             <div>
                 <span>Username:</span>
-                <input />
+                <input ref={usernameRef} type="text"/>
             </div>
             <div>
                 <span>Password:</span>
-                <input />
+                <input ref={passwordRef} type="text"/>
             </div>
-            <button type="submit">Submit</button>
+            <button onClick={submit} type="submit">Submit</button>
         </div>
     )
 }
