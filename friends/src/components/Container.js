ccaf068aa7c90a6da8e42c1e597d6f900bc202d9
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-15 13:17:19 +0100

    Initialize project files
    In this commit, I created a new react app called --friends-- and created a components directory with a barebone <Container /> component

---
+++ friends/src/components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Container(){
+
+    return(
+        <div>
+            Welcome to our Container component
+        </div>
+    )
+}
+
+export default Container;
