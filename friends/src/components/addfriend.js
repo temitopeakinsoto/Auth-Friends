a76acbcffd2987e03aa3e47abce98d9717de6873
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-16 07:03:51 +0100

    Create a new <NewFriend /> component
    In this commit, i created a <NewFriend /> component which will be a form for creating
    new friends. The form fields are name, age and email, which upon submission is routed
    to the api/friends end-point via a post request

---
+++ friends/src/components/addfriend.js
@@ -0,0 +1,41 @@
+import React,{ useRef } from 'react';
+import withAuth from '../axios';
+
+export default function NewFriend(props) {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const ageRef = useRef();
+
+    const submit = () => {
+        withAuth().post("http://localhost:5000/api/friends/", {
+          name: nameRef.current.value,
+          email: emailRef.current.value,
+          age: ageRef.current.value,
+        })
+        .then(res => {
+            props.history.push('/friends');
+        })
+        .catch(error => {
+            alert(error.response.data.message);
+        });
+      };
+
+    return(
+        <div>
+            <div>
+                <h1>Add A New Friend</h1>
+                <span>Name:</span>
+                <input ref={nameRef} type="text"/>
+            </div>
+            <div>
+                <span>Age:</span>
+                <input ref={ageRef} type="text"/>
+            </div>           
+            <div>
+                <span>Email:</span>
+                <input ref={emailRef} type="text"/>
+            </div>
+            <button onClick={submit} type="submit">Submit</button>
+        </div>
+    )
+}
